256b6eddf249a96c078277822fd81d0d5d587598
clarkpham0407@gmail.com <clarkpham0407@gmail.com>
2020-12-23 20:01:50 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        All is reserved
+      </footer>
+    </div>
+  );
+}
+
+export default App;
